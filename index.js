8657f03128654eba63145e68c89f54774a0468b1
napat sangthongthat <napat@git.com>
2021-02-07 09:44:38 +0700

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotnev").config();
+const express=require("express");
+const app=express();
+const port=process.env.PORT;
+app.get("/",(req,res)=>{
+    res.send("Hello world");
+});
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
