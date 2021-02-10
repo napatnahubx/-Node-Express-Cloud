e149c3247de1a5cbcbffe90dc85ba2623d00fe1b
napat sangthongthat <napat@git.com>
2021-02-10 16:13:17 +0700

    Start

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require("dotnev").config();
-const express=require("express");
-const app=express();
-const port=process.env.PORT;
-app.get("/",(req,res)=>{
-    res.send("Hello world");
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello Node.js World!");
 });
-app.listen(port,()=>{
-    console.log(`Example app listening at http://localhost:${port}`);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
